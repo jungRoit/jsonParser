f62f7d70281f74bf421b7cb000d5b37d0314e86f
jungRoit <roit.github@gmail.com>
2020-04-26 18:05:53 +0545

    added constants

--- constants/constants.js
+++ constants/constants.js
@@ -6,3 +6,9 @@ exports.JSOM_WHITESPACE = [' ', '\t', '\b', '\n', '\r'];
 exports.TOKENS = Object.values(JSON_TOKENS);
 
 exports.JSON_NUMBERS = ['0','1','2','3','4','5','6','7','8','9','.','-'];
+
+exports.JSON_TRUE = 'true';
+exports.JSON_FALSE = 'false';
+
+exports.TRUE_LENGTH = 4;
+exports.FALSE_LENGTH = 5;
